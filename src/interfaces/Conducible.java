1be1aba8ea570bc2f3beed8a088e75f325e6a293
MateoR-gif <mateorendon008@gmail.com>
2023-04-18 23:42:37 -0500

    Se añadieron comentarios

--- src/interfaces/Conducible.java
+++ src/interfaces/Conducible.java
@@ -1,7 +1,22 @@
 package interfaces;
 
 public interface Conducible {
+    /**
+     * Description: Este método muestra en pantalla
+     * la acción de conducir dependiendo del vehículo
+     * de la clase instanciada.
+     * */
     void conducir();
+    /**
+     * Description: Este método hace explotar al
+     * vehículo de la clase instanciada.
+     * */
     void explotar();
+    /**
+     * Description: Este método permite agregar
+     * una modificación al vehículo de la clase instanciada.
+     * @param nombreModificacion Se requiere ingresar el nombre
+     *                           de la modificacion.
+     * */
     void añadirModificacion(String nombreModificacion);
 }
