b6dbe14ae73a811990afbefe1f72accb9d77a07a
MateoR-gif <mateorendon008@gmail.com>
2023-04-18 23:01:56 -0500

    Nueva Feature

--- src/interfaces/Conducible.java
+++ src/interfaces/Conducible.java
@@ -1,5 +1,7 @@
 package interfaces;
 
 public interface Conducible {
-    public void conducir();
+    void conducir();
+    void explotar();
+    void añadirModificacion(String nombreModificacion);
 }
